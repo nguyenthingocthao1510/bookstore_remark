8d691e6de5a428448e9ac5fa21d1aaae83e9ccb0
nguyenthingocthao1510 <ntnthao15102002@gmail.com>
2025-07-10 13:51:28 +0700

    feat: create login component

---
+++ bookstore_remake/src/components/Form/FormItem/InputFields.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import type { FormProps } from "antd";
+import { Col, Form } from "antd";
+import classNames from "classnames";
+import { FormikProps } from "formik";
+import FormItem, { InputProps } from "src/components/Form/FormItem";
+
+type Props = Omit<FormProps, "form"> & {
+  inputs: InputProps[];
+  form?: FormikProps<any>;
+  span?: {
+    sm?: number;
+    md?: number;
+    lg?: number;
+    xl?: number;
+  };
+  gutter?: [number, number];
+  isHorizontal?: boolean;
+  onKeyDown?: (e: any) => void;
+};
+
+const InputFields: React.FC<Props> = React.memo(
+  ({
+    inputs,
+    form,
+    gutter,
+    span = { sm: 24, md: 12, lg: 8, xl: 6 },
+    isHorizontal = true,
+    onKeyDown,
+    ...otherProps
+  }) => {
+    const formLayout = isHorizontal ? "horizontal" : "vertical";
+
+    return (
+      <Form
+        {...otherProps}
+        layout={formLayout}
+        className={classNames(otherProps.className, "form-container")}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          alignItems: "flex-start",
+        }}
+      >
+        {inputs.map((input, index) => {
+          const { key, onChange, onBlur, ...inputProps } = input;
+          return (
+            <Col
+              style={{
+                padding: gutter ? `${gutter[0]}px ${gutter[1]}px` : "0",
+              }}
+              className={input.className}
+              key={input.name + index}
+              sm={input.span || span.sm}
+              md={input.span || span.md}
+              lg={input.span || span.lg}
+              xl={input.span || span.xl}
+            >
+              <FormItem
+                key={key}
+                {...inputProps}
+                name={input.name}
+                value={form?.values?.[input.name]}
+                error={
+                  !!form?.touched?.[input.name] && form?.errors?.[input.name]
+                    ? (form?.errors?.[input.name] as string)
+                    : undefined
+                }
+                touched={!!form?.touched?.[input.name]}
+                onChange={(e: any) => {
+                  form?.handleChange(e);
+                  onChange?.(e);
+                }}
+                onBlur={(e: any) => {
+                  onBlur?.(e);
+                  form?.setFieldTouched(input.name, true);
+                  form?.handleBlur(e);
+                }}
+                onKeyDown={onKeyDown}
+              />
+            </Col>
+          );
+        })}
+      </Form>
+    );
+  }
+);
+
+export default InputFields;
--- bookstore_remake/src/components/Form/FormItem/index.tsx
+++ bookstore_remake/src/components/Form/FormItem/index.tsx
@@ -1,318 +1,315 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { TYPE_FIELD } from "../../../constants/enum/common";
-import { Checkbox, Col, Form, Row, theme } from "antd";
+import { Checkbox, Col, Form, Row, theme, Typography } from "antd";
 import TextInput from "./components/TextInput";
 import SelectInput from "./components/SelectInput";
 import RadioInput from "./components/RadioInput";
 import PasswordInput from "./components/PasswordInput";
-import { DatePickerInput, RangePickerInput, WeekPickerInput } from "./components/DateInput";
+import {
+  DatePickerInput,
+  RangePickerInput,
+  WeekPickerInput,
+} from "./components/DateInput";
 import { DATE_FORMAT } from "../../../constants/common/common";
 import AutoCompleteInput from "./components/AutoComplete";
-import './style.scss';
+import "./style.scss";
 import { TOption } from "../../../constants/types/common";
 
 export type InputProps = {
-    name: string;
-    span?: number;
-    key?: string;
-    value: any;
-    type: (typeof TYPE_FIELD)[keyof typeof TYPE_FIELD];
-    icon?: ReactNode;
-    buttonType?: any;
-    isVisible?: boolean;
-    placeholder?: string;
-    label?: string;
+  name: string;
+  span?: number;
+  key?: string;
+  value: any;
+  type: (typeof TYPE_FIELD)[keyof typeof TYPE_FIELD];
+  icon?: ReactNode;
+  buttonType?: any;
+  isVisible?: boolean;
+  placeholder?: string;
+  label?: string;
 
-    require?: boolean;
-    disabled?: boolean;
-    error?: string;
-    touched?: boolean;
+  require?: boolean;
+  disabled?: boolean;
+  error?: string;
+  touched?: boolean;
 
-    options?: TOption[];
-    onChange?: (e: any, dateString?: any) => void;
-    onBlur?: (e: any) => void;
-    onKeyDown?: (e: any) => void;
-    onClick?: (e: any) => void;
-    onSearch?: (e: any) => void;
-    onSelect?: (e: any) => void;
+  options?: TOption[];
+  onChange?: (e: any, dateString?: any) => void;
+  onBlur?: (e: any) => void;
+  onKeyDown?: (e: any) => void;
+  onClick?: (e: any) => void;
+  onSearch?: (e: any) => void;
+  onSelect?: (e: any) => void;
 
-    className?: string;
-    addonAfter?: ReactNode;
-    addonBefore?: ReactNode;
-    size?: 'small' | 'middle' | 'large';
-    isMultiple?: boolean;
-    showTime?: boolean;
-    formatDateTime?: string;
-    showCount?: boolean;
-    allowClear?: boolean;
-    prefix?: any;
-    maxLength?: number;
-    defaultValue?: string | number;
-    suffixIcon?: ReactNode;
-    fileSize?: number;
-    accept?: string;
-    showLanguageToggle?: boolean;
-    showModal?: boolean;
-    onModalOpen?: (e: any) => void;
-    disabledDate?: (current: any) => boolean;
-    checked?: boolean;
-    style: React.CSSProperties;
-    showSearch?: boolean;
-    maxCount?: number;
-}
+  className?: string;
+  addonAfter?: ReactNode;
+  addonBefore?: ReactNode;
+  size?: "small" | "middle" | "large";
+  isMultiple?: boolean;
+  showTime?: boolean;
+  formatDateTime?: string;
+  showCount?: boolean;
+  allowClear?: boolean;
+  prefix?: any;
+  maxLength?: number;
+  defaultValue?: string | number;
+  suffixIcon?: ReactNode;
+  fileSize?: number;
+  accept?: string;
+  showLanguageToggle?: boolean;
+  showModal?: boolean;
+  onModalOpen?: (e: any) => void;
+  disabledDate?: (current: any) => boolean;
+  checked?: boolean;
+  style?: React.CSSProperties;
+  showSearch?: boolean;
+  maxCount?: number;
+};
 
 const FormItem: React.FC<InputProps> = ({
-    defaultValue,
-    type,
-    options,
-    value,
-    onChange,
-    require,
-    formatDateTime,
-    placeholder,
-    className,
-    touched,
-    error,
-    allowClear,
-    size,
-    disabled,
-    label,
-    icon,
-    showTime,
-    addonAfter,
-    addonBefore,
-    showCount,
-    onSearch,
-    onSelect,
-    isMultiple,
-    name,
-    prefix,
-    suffixIcon,
-    maxLength,
-    style,
-    onKeyDown,
-    onClick,
-    maxCount,
-    fileSize,
-    accept,
-    disabledDate,
-    showLanguageToggle,
-    showModal,
-    showSearch,
-    checked,
-    buttonType,
-    ...otherProps
+  defaultValue,
+  type,
+  options,
+  value,
+  onChange,
+  require,
+  formatDateTime,
+  placeholder,
+  className,
+  touched,
+  error,
+  allowClear,
+  size,
+  disabled,
+  label,
+  icon,
+  showTime,
+  addonAfter,
+  addonBefore,
+  showCount,
+  onSearch,
+  onSelect,
+  isMultiple,
+  name,
+  prefix,
+  suffixIcon,
+  maxLength,
+  style,
+  onKeyDown,
+  onClick,
+  maxCount,
+  fileSize,
+  accept,
+  disabledDate,
+  showLanguageToggle,
+  showModal,
+  showSearch,
+  checked,
+  buttonType,
+  ...otherProps
 }) => {
-    const { token } = theme.useToken();
+  const { token } = theme.useToken();
 
-    const renderItem = () => {
-        switch (type) {
-            case TYPE_FIELD.TEXT:
-                return (
-                    <div>
-                        <TextInput
-                            name={name}
-                            style={style}
-                            type={type}
-                            value={value}
-                            showModal={showModal}
-                            error={error}
-                            onChange={onChange}
-                            onClick={onClick}
-                            onKeyDown={onKeyDown}
-                            touched={touched}
-                            placeholder={placeholder}
-                            allowClear={allowClear}
-                            size={size}
-                            defaultValue={defaultValue}
-                            disabled={disabled}
-                            suffixIcon={suffixIcon}
-                            addonAfter={addonAfter}
-                            addonBefore={suffixIcon}
-                            prefix={prefix}
-                            showLanguageToggle={showLanguageToggle}
-                        ></TextInput>
-                    </div>
-                );
-            case TYPE_FIELD.SELECT:
-                return (
-                    <div>
-                        <SelectInput
-                            disabled={disabled}
-                            size={'small'}
-                            name={name}
-                            mode={isMultiple ? 'multiple' : undefined}
-                            options={options || []}
-                            className={className}
-                            placeholder={placeholder}
-                            value={value}
-                            onChange={onChange}
-                            showSearch={showSearch}
-                            allowClear={allowClear}
-                            onKeyDown={onKeyDown}
-                            onClick={onClick}
-                        ></SelectInput>
-                    </div>
-                );
-            case TYPE_FIELD.RADIO:
-                return (
-                    <div>
-                        <RadioInput
-                            name={name}
-                            value={value}
-                            disabled={disabled}
-                            onChange={onChange}
-                            options={options ?? []}
-                        ></RadioInput>
-                    </div>
-                );
-            case TYPE_FIELD.PASSWORD:
-                return (
-                    <div>
-                        <PasswordInput
-                            name={name}
-                            onChange={onChange}
-                            size='small'
-                            placeholder={placeholder ?? 'Nhập'}
-                            disabled={disabled}
-                            value={value}
-                        ></PasswordInput>
-                    </div>
-                );
-            case TYPE_FIELD.DATE_PICKER:
-                return (
-                    <div>
-                        <DatePickerInput
-                            name={name}
-                            value={value}
-                            className={className}
-                            onChange={onChange}
-                            formatDatetime={formatDateTime || DATE_FORMAT.DATE}
-                            style={style}
-                            showTime={showTime}
-                            disabled={disabled}
-                            placeholder={placeholder}
-                            disabledDate={disabledDate}
-                            size={'small'}
-                        ></DatePickerInput>
-                    </div>
-                );
-            case TYPE_FIELD.WEEK_PICKER:
-                return (
-                    <div>
-                        <WeekPickerInput
-                            value={value}
-                            name={name}
-                            onChange={onChange}
-                            size={size}
-                            placeholder={placeholder} style={style}
-                        ></WeekPickerInput>
-                    </div>
-                );
-            case TYPE_FIELD.RANGE_PICKER:
-                return (
-                    <div>
-                        <RangePickerInput
-                            name={name}
-                            value={value}
-                            onChange={onChange}
-                            className={className}
-                            suffixIcon={suffixIcon}
-                            formatDatetime={formatDateTime}
-                            style={style}
-                            size={size}
-                            allowClear={allowClear}
-                        ></RangePickerInput>
-                    </div>
-                );
-            case TYPE_FIELD.AUTO_COMPLETE:
-                return (
-                    <div>
-                        <AutoCompleteInput
-                            allowClear={allowClear}
-                            backfill={true}
-                            className={className}
-                            disabled={disabled}
-                            value={value}
-                            size={'small'}
-                            placeholder={placeholder || 'Nhập'}
-                            onSearch={onSearch}
-                            onSelect={onSelect}
-                            onChange={onChange}
-                            options={options ?? []}
-                            onClick={onClick}
-                        ></AutoCompleteInput>
-                    </div>
-                )
+  const renderItem = () => {
+    switch (type) {
+      case TYPE_FIELD.TEXT:
+        return (
+          <div>
+            <div>
+              {label ? <label className={name}>{label}</label> : null}
+              <TextInput
+                name={name}
+                style={style}
+                type={type}
+                value={value}
+                showModal={showModal}
+                error={error}
+                onChange={onChange}
+                onClick={onClick}
+                onKeyDown={onKeyDown}
+                touched={touched}
+                placeholder={placeholder}
+                allowClear={allowClear}
+                size={size}
+                defaultValue={defaultValue}
+                disabled={disabled}
+                suffixIcon={suffixIcon}
+                addonAfter={addonAfter}
+                addonBefore={suffixIcon}
+                prefix={prefix}
+                showLanguageToggle={showLanguageToggle}
+              ></TextInput>
+            </div>
+          </div>
+        );
+      case TYPE_FIELD.SELECT:
+        return (
+          <div>
+            <SelectInput
+              disabled={disabled}
+              size={"small"}
+              name={name}
+              mode={isMultiple ? "multiple" : undefined}
+              options={options || []}
+              className={className}
+              placeholder={placeholder}
+              value={value}
+              onChange={onChange}
+              showSearch={showSearch}
+              allowClear={allowClear}
+              onKeyDown={onKeyDown}
+              onClick={onClick}
+            ></SelectInput>
+          </div>
+        );
+      case TYPE_FIELD.RADIO:
+        return (
+          <div>
+            <RadioInput
+              name={name}
+              value={value}
+              disabled={disabled}
+              onChange={onChange}
+              options={options ?? []}
+            ></RadioInput>
+          </div>
+        );
+      case TYPE_FIELD.PASSWORD:
+        return (
+          <div>
+            {label ? <label className={name}>{label}</label> : null}
+            <PasswordInput
+              name={name}
+              onChange={onChange}
+              size="small"
+              placeholder={placeholder ?? "Nhập"}
+              disabled={disabled}
+              value={value}
+              allowClear={allowClear}
+              style={style}
+            ></PasswordInput>
+          </div>
+        );
+      case TYPE_FIELD.DATE_PICKER:
+        return (
+          <div>
+            <DatePickerInput
+              name={name}
+              value={value}
+              className={className}
+              onChange={onChange}
+              formatDatetime={formatDateTime || DATE_FORMAT.DATE}
+              style={style}
+              showTime={showTime}
+              disabled={disabled}
+              placeholder={placeholder}
+              disabledDate={disabledDate}
+              size={"small"}
+            ></DatePickerInput>
+          </div>
+        );
+      case TYPE_FIELD.WEEK_PICKER:
+        return (
+          <div>
+            <WeekPickerInput
+              value={value}
+              name={name}
+              onChange={onChange}
+              size={size}
+              placeholder={placeholder}
+              style={style}
+            ></WeekPickerInput>
+          </div>
+        );
+      case TYPE_FIELD.RANGE_PICKER:
+        return (
+          <div>
+            <RangePickerInput
+              name={name}
+              value={value}
+              onChange={onChange}
+              className={className}
+              suffixIcon={suffixIcon}
+              formatDatetime={formatDateTime}
+              style={style}
+              size={size}
+              allowClear={allowClear}
+            ></RangePickerInput>
+          </div>
+        );
+      case TYPE_FIELD.AUTO_COMPLETE:
+        return (
+          <div>
+            <AutoCompleteInput
+              allowClear={allowClear}
+              backfill={true}
+              className={className}
+              disabled={disabled}
+              value={value}
+              size={"small"}
+              placeholder={placeholder || "Nhập"}
+              onSearch={onSearch}
+              onSelect={onSelect}
+              onChange={onChange}
+              options={options ?? []}
+              onClick={onClick}
+            ></AutoCompleteInput>
+          </div>
+        );
 
-            case TYPE_FIELD.CHECKBOX:
-                return (
-                    <div>
-                        {options ? (
-                            <Checkbox.Group
-                                disabled={disabled}
-                                onChange={onChange}
-                                name={name}
-                                value={value}
-                            >
-                                <Row>
-                                    {options?.map((item: any, index: any) => (
-                                        <Col key={index} span={item.span}>
-                                            <Checkbox
-                                                checked={checked}
-                                                value={item.value}
-                                                style={{
-                                                    width: '100%'
-                                                }}
-                                            >
-                                                {item.label}
-                                            </Checkbox>
-                                        </Col>
-                                    ))}
-                                </Row>
-                            </Checkbox.Group>
-                        ) : (
-                            <Checkbox
-                                checked={checked}
-                                value={value}
-                                onChange={onChange}
-                                style={{
-                                    width: '100%'
-                                }}
-                            ></Checkbox>
-                        )}
-                    </div>
-                )
-            default:
-                break;
-        }
-    };
+      case TYPE_FIELD.CHECKBOX:
+        return (
+          <div>
+            {options ? (
+              <Checkbox.Group
+                disabled={disabled}
+                onChange={onChange}
+                name={name}
+                value={value}
+              >
+                <Row>
+                  {options?.map((item: any, index: any) => (
+                    <Col key={index} span={item.span}>
+                      <Checkbox
+                        checked={checked}
+                        value={item.value}
+                        style={{
+                          width: "100%",
+                        }}
+                      >
+                        {item.label}
+                      </Checkbox>
+                    </Col>
+                  ))}
+                </Row>
+              </Checkbox.Group>
+            ) : (
+              <Checkbox
+                checked={checked}
+                value={value}
+                onChange={onChange}
+                style={{
+                  width: "100%",
+                }}
+              ></Checkbox>
+            )}
+          </div>
+        );
+      default:
+        break;
+    }
+  };
 
-    return (
-        <Form.Item
-            {...otherProps}
-            name={name}
-            className="form-item-container"
-            label={
-                label ? (
-                    <>
-                        {require ? (
-                            <>
-                                {label}
-                                <span style={{ color: '#F25B60', marginLeft: '5px' }}>*</span>
-                            </>
-                        ) : (
-                            label
-                        )}
-                    </>
-                ) : null
-            }
-            validateStatus={touched && error ? 'error' : ''}
-            help={touched && error}
-        >
-            {renderItem()}
-        </Form.Item >
-    )
+  return (
+    <Form.Item
+      {...otherProps}
+      name={name}
+      className="form-item-container"
+      validateStatus={touched && error ? "error" : ""}
+      help={touched && error}
+    >
+      {renderItem()}
+    </Form.Item>
+  );
 };
 
 export default FormItem;
